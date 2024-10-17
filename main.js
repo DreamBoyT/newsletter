6d1c041d3d791b176db3e23d8bc290d8a90e071d
Tamizhselvan R D <115417933+DreamBoyT@users.noreply.github.com>
2024-10-17 23:59:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  document.querySelector('.subscription').classList.add('done');
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwG79-Je3mjIWYeDvCU5TsCp6Ha8X3O27Yk8vQaFjuzeY4ldrwWJUiTODuojzo6_8SKeg/exec';
-  const form = document.querySelector('form[name="contact-form"]');
-
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, {
-        method: 'post',
-        body: new FormData(form)
-      })
-      .then(response => alert("Thank you! Your form is submitted successfully."))
-      .then(() => {
-        window.location.reload();
-      })
-      .catch(error => console.error('Error!', error.message));
-  });
-});
+document.addEventListener('DOMContentLoaded', function() {  
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwG79-Je3mjIWYeDvCU5TsCp6Ha8X3O27Yk8vQaFjuzeY4ldrwWJUiTODuojzo6_8SKeg/exec';  
+  const form = document.querySelector('form[name="contact-form"]');  
+    
+  form.addEventListener('submit', e => {  
+    if (!form['your-email'].value) {  
+      alert('Please enter your email address.');  
+      return;  
+    }  
+    e.preventDefault();  
+    fetch(scriptURL, {  
+        method: 'post',  
+        body: new FormData(form)  
+      })  
+      .then(response => alert("Thank you! Your form is submitted successfully."))  
+      .then(() => {  
+        window.location.reload();  
+      })  
+      .catch(error => console.error('Error!', error.message));  
+  });  
+}); 
